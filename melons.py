4ed7c29bae3e356916a1f0f2b5e88af28de11d1b
vagrant <vagrant@vagrant.vm>
2020-05-08 19:26:23 +0000

    Modified get_total() based on updated shipping costs

--- melons.py
+++ melons.py
@@ -3,7 +3,8 @@
 class AbstractMelonOrder():
     """An abstract class base for melon orders to inherit from."""
 
-    self.shipped = False
+    shipped = False
+    is_christmas = False
 
     def __init__(self, species, qty):
         self.species = species
@@ -15,32 +16,36 @@ class AbstractMelonOrder():
         base_price = 5
         total = (1 + self.tax) * self.qty * base_price
 
-        return total
+        return (1.5 * total if self.is_christmas else total)
 
     def mark_shipped(self):
         """Record the fact than an order has been shipped."""
 
         self.shipped = True
 
+    def mark_is_christmas():
 
-class DomesticMelonOrder():
+        self.is_christmas = True
+
+
+class DomesticMelonOrder(AbstractMelonOrder):
     """A melon order within the USA."""
 
     def __init__(self, species, qty):
         """Initialize melon order attributes."""
 
-        super.().__init__(self, species, qty)
+        super().__init__(self, species, qty)
         self.order_type = "domestic"
         self.tax = 0.08
 
     
-class InternationalMelonOrder():
+class InternationalMelonOrder(AbstractMelonOrder):
     """An international (non-US) melon order."""
 
     def __init__(self, species, qty, country_code):
         """Initialize melon order attributes."""
 
-        super.().__init__(self, species, qty)
+        super().__init__(species, qty)
         self.country_code = country_code
         self.order_type = "international"
         self.tax = 0.17
@@ -50,3 +55,11 @@ class InternationalMelonOrder():
         """Return the country code."""
 
         return self.country_code
+
+    def get_total(self):
+        """Calculate price, including tax"""
+
+        return (super().get_total() 
+                if self.qty >= 10 
+                else (super().get_total() + 3.00))
+
