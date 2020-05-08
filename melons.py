f96334d856c1a68c078e94070f0c71ee8e88ef91
vagrant <vagrant@vagrant.vm>
2020-05-08 19:09:02 +0000

    Created AbstractMelonOrder class and refactored

--- melons.py
+++ melons.py
@@ -1,5 +1,27 @@
 """Classes for melon orders."""
 
+class AbstractMelonOrder():
+    """An abstract class base for melon orders to inherit from."""
+
+    self.shipped = False
+
+    def __init__(self, species, qty):
+        self.species = species
+        self.qty = qty
+
+    def get_total(self):
+        """Calculate price, including tax."""
+
+        base_price = 5
+        total = (1 + self.tax) * self.qty * base_price
+
+        return total
+
+    def mark_shipped(self):
+        """Record the fact than an order has been shipped."""
+
+        self.shipped = True
+
 
 class DomesticMelonOrder():
     """A melon order within the USA."""
@@ -7,51 +29,22 @@ class DomesticMelonOrder():
     def __init__(self, species, qty):
         """Initialize melon order attributes."""
 
-        self.species = species
-        self.qty = qty
-        self.shipped = False
+        super.().__init__(self, species, qty)
         self.order_type = "domestic"
         self.tax = 0.08
 
-    def get_total(self):
-        """Calculate price, including tax."""
-
-        base_price = 5
-        total = (1 + self.tax) * self.qty * base_price
-
-        return total
-
-    def mark_shipped(self):
-        """Record the fact than an order has been shipped."""
-
-        self.shipped = True
-
-
+    
 class InternationalMelonOrder():
     """An international (non-US) melon order."""
 
     def __init__(self, species, qty, country_code):
         """Initialize melon order attributes."""
 
-        self.species = species
-        self.qty = qty
+        super.().__init__(self, species, qty)
         self.country_code = country_code
-        self.shipped = False
         self.order_type = "international"
         self.tax = 0.17
 
-    def get_total(self):
-        """Calculate price, including tax."""
-
-        base_price = 5
-        total = (1 + self.tax) * self.qty * base_price
-
-        return total
-
-    def mark_shipped(self):
-        """Record the fact than an order has been shipped."""
-
-        self.shipped = True
 
     def get_country_code(self):
         """Return the country code."""
